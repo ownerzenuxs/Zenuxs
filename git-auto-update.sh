71e445fa5f136b864ac49c49b32c8fd76d35f4ba
ownerzenuxs <iwantvpspls@gmail.com>
2025-04-04 11:05:22 +0000

    Auto-update: Fri Apr  4 11:05:22 UTC 2025

--- git-auto-update.sh
+++ git-auto-update.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-cd /home/container/github-files
+
+cd /home/container/github-files || exit
+
+# Ensure correct Git user details
+git config --global user.email "iwantvpspls@gamil.com"
+git config --global user.name "ownerzenuxs"
+
+# Add changes and commit
 git add .
-git commit -m "Auto-updated files from Tiny File Manager"
+git commit -m "Auto-update: $(date)"
+
+# Push to GitHub
 git push origin main
